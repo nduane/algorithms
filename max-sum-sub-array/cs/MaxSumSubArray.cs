062827b95792389a256a9fe008aa9eaa4f3a31a2
nick <nick@plankton.fios-router.home>
2017-04-16 11:45:16 -0400

    Fixed bug when numbers are all negative

--- max-sum-sub-array/cs/MaxSumSubArray.cs
+++ max-sum-sub-array/cs/MaxSumSubArray.cs
@@ -32,10 +32,10 @@ public class Application
 					}
 				}
 
-			// Else if our sum as gone to less than or equal to zero
-			// then we can start a new running sum at the next index.
+			// If our sum as gone to less than or equal to zero then we
+			// can start a new running sum at the next index.
 
-			else if (curSum <= 0)
+			if (curSum <= 0)
 				{
 				curSum = 0;
 				curStart = i + 1;
@@ -48,8 +48,8 @@ public class Application
 		return maxSum;
 	}
 
-	private static int MaxSumSubArrayOrderNSquared(int[] values, out uint start,
-			out uint end)
+	private static int MaxSumSubArrayOrderNSquared(int[] values,
+			out uint start, out uint end)
 	{
 		int maxSum = int.MinValue;
 
@@ -85,6 +85,10 @@ public class Application
 	public static void Main(string[] args)
 	{
 //		int[] values = new int[] {30, -25, -6, 8, 23, 0};
+//		int[] values1 = new int[] {-30, -40, -50, -60, -7, -3, -100, -30, -70};
+//		int[] values2 = new int[] {-30, -40, -50, -60, -7, -3, 20, -19, 1, -3};
+//		int[] values3 = new int[] {-30, -40, -50, -60, -7, -3, 20, -19, 1, 19, -10};
+
 		int[] values = new int[128 * 1024];
 
 		// Initialize values.
@@ -119,5 +123,69 @@ public class Application
 		Console.WriteLine("order n, max sum: {0}, start: {1}, end: {2}, " +
 				"duration: {3}", sum, start, end,
 				stopwatch.ElapsedMilliseconds);
+/*
+		uint start;
+		uint end;
+
+		Stopwatch stopwatch = new Stopwatch();
+
+		stopwatch.Start();
+		int sum = MaxSumSubArrayOrderNSquared(values1, out start, out end);
+		stopwatch.Stop();
+		Console.WriteLine("1: order nsquared, max sum: {0}, start: {1}, " +
+				"end: {2}, duration: {3}", sum, start, end,
+				stopwatch.ElapsedMilliseconds);
+
+		stopwatch.Reset();
+	
+		// Order n.
+
+		stopwatch.Start();
+		sum = MaxSumSubArrayOrderN(values1, out start, out end);
+		stopwatch.Stop();
+		Console.WriteLine("1: order n, max sum: {0}, start: {1}, end: " +
+				"{2}, duration: {3}", sum, start, end,
+				stopwatch.ElapsedMilliseconds);
+
+		stopwatch.Reset();
+
+		stopwatch.Start();
+		sum = MaxSumSubArrayOrderNSquared(values2, out start, out end);
+		stopwatch.Stop();
+		Console.WriteLine("2: order nsquared, max sum: {0}, start: {1}, " +
+				"end: {2}, duration: {3}", sum, start, end,
+				stopwatch.ElapsedMilliseconds);
+
+		stopwatch.Reset();
+	
+		// Order n.
+
+		stopwatch.Start();
+		sum = MaxSumSubArrayOrderN(values2, out start, out end);
+		stopwatch.Stop();
+		Console.WriteLine("2: order n, max sum: {0}, start: {1}, end: " +
+				"{2}, duration: {3}", sum, start, end,
+				stopwatch.ElapsedMilliseconds);
+
+		stopwatch.Reset();
+	
+		stopwatch.Start();
+		sum = MaxSumSubArrayOrderNSquared(values3, out start, out end);
+		stopwatch.Stop();
+		Console.WriteLine("3: order nsquared, max sum: {0}, start: {1}, " +
+				"end: {2}, duration: {3}", sum, start, end,
+				stopwatch.ElapsedMilliseconds);
+
+		stopwatch.Reset();
+	
+		// Order n.
+
+		stopwatch.Start();
+		sum = MaxSumSubArrayOrderN(values3, out start, out end);
+		stopwatch.Stop();
+		Console.WriteLine("3: order n, max sum: {0}, start: {1}, end: " +
+				"{2}, duration: {3}", sum, start, end,
+				stopwatch.ElapsedMilliseconds);
+*/
 	}
 }
